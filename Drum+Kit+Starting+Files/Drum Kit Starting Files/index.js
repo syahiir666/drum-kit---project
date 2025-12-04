031b16cd7491cbef284bb2a77203df74d320c55d
syahiir666@gmail.com <syahiir666@gmail.com>
2025-12-05 00:28:08 +0800

    first commit

---
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,54 @@
+
+document.querySelectorAll(".drum").forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        var buttonText = this.innerHTML;
+        makeSound(buttonText);
+        buttonAnimation(buttonText)
+    });
+});
+
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);   
+    buttonAnimation(event.key);
+});
+
+
+function makeSound(key) {
+    switch(key) {
+        case "w":
+            new Audio('sounds/tom-1.mp3').play();
+            break;
+        case "a":
+            new Audio('sounds/tom-2.mp3').play();
+            break;
+        case "s":
+            new Audio('sounds/tom-3.mp3').play();
+            break;
+        case "d":
+            new Audio('sounds/tom-4.mp3').play();
+            break;
+        case "j":
+            new Audio('sounds/snare.mp3').play();
+            break;
+        case "k":
+            new Audio('sounds/crash.mp3').play();
+            break;
+        case "l":
+            new Audio('sounds/kick-bass.mp3').play();
+            break;
+        default:
+            console.log(buttonText);
+    }
+}
+
+function buttonAnimation(currentkey) {
+
+    var activeButton = document.querySelector("." + currentkey);
+
+    activeButton.classList.add("pressed")
+
+    setTimeout(function() {
+            activeButton.classList.remove("pressed");
+        }, 100);
+}
